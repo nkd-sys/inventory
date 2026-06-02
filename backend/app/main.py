11f94da6a8a803a63b41eafc72856b187831cf80
n001kumardubey <n001kumardubey@gmail.com>
2026-06-02 23:22:08 +0530

    inventory part1

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Inventory & Order Management API", version="1.0.0")
 
-origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+origins = os.getenv("ALLOWED_ORIGINS", "https://helpful-travesseiro-b82b98.netlify.app/").split(",")
 
 app.add_middleware(
     CORSMiddleware,
